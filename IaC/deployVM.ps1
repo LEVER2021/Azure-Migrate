a5c83a8cf2b19c23e1a71b2313c1dc16c341b76f
EirikOscar <eirik.oscar.horgen.ness@gmail.com>
2021-02-07 13:45:07 +0100

    deploy vm ps

---
+++ IaC/deployVM.ps1
@@ -0,0 +1,13 @@
+﻿Connect-AzAccount
+$tempalteFile = "C:\Users\eirik\OneDrive - NTNU\Bachelor21\Main\hyper-V\Azure\VM\template.json"
+$parameterFile = "C:\Users\eirik\OneDrive - NTNU\Bachelor21\Main\hyper-V\Azure\VM\parameters.json"
+$adminPassword = Read-Host -Prompt "Enter admin password" -AsSecureString
+New-AzResourceGroup `
+    -Name automateDeployment `
+    -Location "North Europe"
+New-AzResourceGroupDeployment `
+    -Name devenvironment `
+    -ResourceGroupName automateDeployment `
+    -TemplateFile $tempalteFile `
+    -TemplateParameterFile $parameterFile `
+    -adminPassword $adminPassword
