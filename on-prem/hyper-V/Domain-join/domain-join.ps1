37d350bab8c1a8272e4c9562fbc3c8ba997a478a
EirikOscar <eirik.oscar.horgen.ness@gmail.com>
2021-02-11 16:03:07 +0100

    kommentar på domenefil

--- on-prem/hyper-V/Domain-join/domain-join.ps1
+++ on-prem/hyper-V/Domain-join/domain-join.ps1
@@ -1,6 +1,7 @@
 
-#Legge maskiner inn i domenet
-foreach($c in Get-NetAdapter) {
+#Legge maskiner inn i domenet 
+#Først endre dns til domenekontroller, så legge til i domenet
+foreach ($c in Get-NetAdapter) {
     Set-DnsClientServerAddress -InterfaceIndex $c.interfaceindex -ServerAddresses ('192.168.0.11')
 }
 Add-Computer -DomainName "LEVER.no" -restart
