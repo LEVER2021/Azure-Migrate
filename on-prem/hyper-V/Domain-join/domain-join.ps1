5effc2159de8f0859c60be36690c8aa9609dff6c
Kristine Løvjomås <kristinelovjomas@Kristines-MacBook-Air.local>
2021-02-11 16:14:25 +0100

    test-push

--- on-prem/hyper-V/Domain-join/domain-join.ps1
+++ on-prem/hyper-V/Domain-join/domain-join.ps1
@@ -1,6 +1,6 @@
 
-#Legge maskiner inn i domenet 
-#Først endre dns til domenekontroller, så legge til i domenet
+#Legge maskiner inn i domenet.
+#Først endre dns til domenekontroller, så legge til i domenet.
 foreach ($c in Get-NetAdapter) {
     Set-DnsClientServerAddress -InterfaceIndex $c.interfaceindex -ServerAddresses ('192.168.0.11')
 }
