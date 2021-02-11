e7a80e5db1176d92a159e7ca7552ba37125cc506
Kristine Løvjomås <kristinelovjomas@Kristines-MacBook-Air.local>
2021-02-11 15:40:17 +0100

    la til innmelding i domene

--- on-prem/hyper-V/Domain-join/domain-join.ps1
+++ on-prem/hyper-V/Domain-join/domain-join.ps1
@@ -1,1 +1,6 @@
-Test-AdlAnalyticsAccount
+
+#Legge maskiner inn i domenet
+foreach($c in Get-NetAdapter) {
+    Set-DnsClientServerAddress -InterfaceIndex $c.interfaceindex -ServerAddresses ('192.168.0.11')
+}
+Add-Computer -DomainName "LEVER.no" -restart
